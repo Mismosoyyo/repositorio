535be5f61c9edbade7e45ac9d9c10ffc2d74e6ac
unknown <dgrandab@gmail.com>
2020-10-06 11:44:57 +0200

    ejercicios

---
+++ u1e3_geometria/formulas.js
@@ -0,0 +1,47 @@
+
+function Area(a,b){ 
+
+            return (b * a)/2;
+            }
+
+ function perimetroTrianguloEquilatero (l) {
+               
+              return  (l*3);
+        
+            }
+
+function perimetroTrianguloIsosceles( l1,l2){            
+            return (2*(l1+l2));
+            
+            }
+            
+function permietroTrianguloEscaleno(l1,l2,l3){
+            return(l1+l2+l3);
+            }
+
+function areaCuadrado(l) {
+            
+            
+            return(l*l);
+            }
+function  perimetroCuadrado(l){
+
+    return(l1+l2);
+}
+function  areaRectangulo(l1,l2){
+    return (l1*l2);
+}
+      
+function longitudCircunferencia(r){
+
+    return (2*Math.PI*r);
+}
+
+function areaCirculo(r){
+ var c=r*r;
+return (Math.PI*c);
+
+}
+
+
+        
---
+++ u1e4_funcionesanonimas/datos.js
@@ -0,0 +1,7 @@
+var b=20;
+ var a=25;
+ var l=10;
+ var l1=25;
+ var l2=26;
+ var l3=29;
+ var r=75;
